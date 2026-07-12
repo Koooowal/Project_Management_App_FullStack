79cf25f9dd7d2d44146ab02d0b8da4fcc10a2e40
Kowal <133172565+Koooowal@users.noreply.github.com>
2026-07-12 22:09:36 +0200

    style: make navbar responsive with mobile hamburger menu

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
 
 export default function Layout() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <main className="max-w-7xl mx-auto px-4 py-6">
+      <Navbar />
+      <main className="mx-auto max-w-7xl px-4 py-6">
         <Outlet />
       </main>
     </div>
